54afba20cd251492ead8ef93c18e8e78fdecba4a
Sophanatprime <55335597+Sophanatprime@users.noreply.github.com>
2022-07-30 22:58:41 +0800

    对 fixdif 宏包提供支持。几个小改进。

--- physicx.bat
+++ physicx.bat
@@ -5,7 +5,7 @@ set TEMP=%PHYSICX%.xdv %PHYSICX%.aux %PHYSICX%.log %PHYSICX%.idx %PHYSICX%.ind %
 set TEX=lualatex
 %TEX% %PHYSICX%.ins
 
-set MODE=-interaction=nonstopmode -synctex=1 -file-line-error
+set MODE=-interaction=errorstopmode -synctex=1 -file-line-error
 set MAKEINDEX=makeindex
 %TEX% %MODE% %PHYSICX%.dtx
 %MAKEINDEX% -s gind.ist -o %PHYSICX%.ind %PHYSICX%.idx
@@ -17,8 +17,8 @@ del %TEMP%
 set TEX=xelatex
 set PHYSICX=physicx-cn
 set TEMP=%PHYSICX%.xdv %PHYSICX%.aux %PHYSICX%.log %PHYSICX%.idx %PHYSICX%.ind %PHYSICX%.ilg %PHYSICX%.out %PHYSICX%.toc %PHYSICX%.glo %PHYSICX%.gls %PHYSICX%.hd %PHYSICX%.xsim %PHYSICX%.lmc %PHYSICX%.listing
-set NOPDFMODE=-interaction=nonstopmode -synctex=1 -file-line-error --no-pdf
-set MODE=-interaction=nonstopmode -synctex=1 -file-line-error -pdf
+set NOPDFMODE=-interaction=errorstopmode -synctex=1 -file-line-error --no-pdf
+set MODE=-interaction=errorstopmode -synctex=1 -file-line-error -pdf
 %TEX% %NOPDFMODE% %PHYSICX%.tex
 %MAKEINDEX% -s gind.ist -o %PHYSICX%.ind %PHYSICX%.idx
 %MAKEINDEX% -s gglo.ist -o %PHYSICX%.gls %PHYSICX%.glo
